3f47fc397a480a2ab92bd9b2593d83ab9c03b1d4
benmccormack <c19459424@mytudublin.ie>
2022-11-26 15:33:40 +0000

    rxjs event handling added for editing and deleting note

--- problem-1/index.js
+++ problem-1/index.js
@@ -36,19 +36,17 @@ function createNote() {
         let deleteButton = document.createElement("button");
         deleteButton.className = "delete";
         deleteButton.textContent = "Delete Note";
+        fromEvent(deleteButton, 'click')
+            .subscribe(() => deleteNote(div.id));
         div.appendChild(deleteButton);
-        deleteButton.onclick = function () {
-        deleteNote(div.id);
-        };
     
         //edit note button
         let editButton = document.createElement("button");
         editButton.className = "edit";
         editButton.textContent = "Edit Note";
+        fromEvent(editButton, 'click')
+            .subscribe(()=> editNote(div.id));
         div.append(editButton);
-        editButton.onclick = function () {
-        editNote(div.id);
-        }
     }
     else {
         alert("Cannot create empty note, please enter some text.")
@@ -56,3 +54,41 @@ function createNote() {
     
     }
 
+function deleteNote(noteID) {
+    //note is retrieved using its unique ID and deleted.
+    note = document.getElementById(noteID);
+    note.remove();
+    };
+
+function editNote(noteID) {
+    //asks user for updated text
+    let text = prompt("Please enter updated text");
+    if (text != "" || null) {
+        //note is retrieved and the text is changed.
+        let note = document.getElementById(noteID);
+        note.textContent = text;
+        let spacer = document.createElement("br");
+        note.appendChild(spacer);
+    
+        //delete note button
+        let deleteButton = document.createElement("button");
+        deleteButton.className = "delete";
+        deleteButton.textContent = "Delete Note";
+        note.appendChild(deleteButton);
+        deleteButton.onclick = function () {
+        deleteNote(note.id);
+        };
+    
+        //edit note button
+        let editButton = document.createElement("button");
+        editButton.className = "edit";
+        editButton.textContent = "Edit Note";
+        note.append(editButton);
+        editButton.onclick = function () {
+        editNote(note.id);
+        }
+    }
+    else {
+        alert("Cannot create empty note, please enter some text.")
+    }
+    }
